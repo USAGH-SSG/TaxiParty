f3fe0eec4a1b9ba882f7c02a12d751a524d34295
namsengi11 <105529834+namsengi11@users.noreply.github.com>
2024-02-19 14:31:31 +0000

    Restructured models for taxiparty

--- taxiparty/forms.py
+++ taxiparty/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import TaxiParty
+from .models import TaxiParty, Route
 
 class TaxiPartyForm(forms.ModelForm):
     class Meta:
@@ -11,3 +11,16 @@ class TaxiPartyForm(forms.ModelForm):
             'route',
             'rider',
         ]
+
+class RouteForm(forms.ModelForm):
+    class Meta:
+        model = Route
+        fields = [
+            'origin',
+            'destination'
+        ]
+    
+    def is_valid(self) -> bool:
+        if self.origin == self.destination:
+            raise forms.ValidationError("Origin and destination cannot be identical")
+        return super().is_valid()
--- taxiparty/models.py
+++ taxiparty/models.py
@@ -1,26 +1,23 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 import datetime
 
 # Create your models here.
+class Location(models.Model):
+    name = models.TextField(blank=False, unique=True)
+
+class Route(models.Model):
+    origin = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='route_origin')
+    destination = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='route_destination')
+    
 class TaxiParty(models.Model):
     date = models.DateField(blank=False, default=datetime.date.today)
     time = models.TimeField(blank=False, default=datetime.time(8, 00))
-    route = models.TextField(blank=False)
-    rider = models.TextField(blank=False)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    rider = models.ManyToManyField(User)
 
     def getAbsoluteUrl(self):
         return reverse("taxiparty:taxipartydynamic", kwargs={"id": self.id})
-
-
-class Route():
-    origin = models.TextField(blank=False)
-    destination = models.TextField(blank=False)
-
-class Rider():
-    rider1 = models.TextField(blank=False)
-    rider2 = models.TextField()
-    rider3 = models.TextField()
-    rider4 = models.TextField()
