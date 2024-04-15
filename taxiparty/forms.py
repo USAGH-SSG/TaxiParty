ed5204dbbfffc9d42c16c61aecb841d2a06a47ac
namsengi11 <105529834+namsengi11@users.noreply.github.com>
2024-04-15 03:36:10 +0000

    Remove legacy code

--- taxiparty/forms.py
+++ taxiparty/forms.py
@@ -13,27 +13,9 @@ class TaxiPartyForm(forms.ModelForm):
 
         ]
 
-    # def is_valid(self) -> bool:
-    #     if self.fields['origin'] == self.fields['destination']:
-    #         raise forms.ValidationError("Origin and destination cannot be identical")
-    #     return super().is_valid()
-
 class LocationForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = [
             'name'
         ]
-
-# class RouteForm(forms.ModelForm):
-#     class Meta:
-#         model = Route
-#         fields = [
-#             'origin',
-#             'destination'
-#         ]
-    
-#     def is_valid(self) -> bool:
-#         if self['origin'] == self['destination']:
-#             raise forms.ValidationError("Origin and destination cannot be identical")
-#         return super().is_valid()
